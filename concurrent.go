eebe87cb2e89e480d24ff94e3965ddc7e0728123
zizaimengzhongyue <luyanfeng1992@qq.com>
2020-08-13 13:31:24 +0800

    concurrent

--- concurrent.go
+++ concurrent.go
@@ -1,6 +1,6 @@
 /**
  * 编写一个函数，入参为 arr []int, num int, fn func(int) int，需求时使用不超过 num 个 goroutine 对 arr 中每个元素执行 fn，并返回执行后的 arr
- * 这是今天面试的一道题目，面试的时候卡在执行计算的几个 goroutine 了
+ * 这是今天面试的一道题目，面试的时候卡在执行计算的几个 goroutine  怎么退出了
  **/
 package main
 
